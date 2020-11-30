0d30f67e2ecd1ab0b20a469838ca22e1b754b2bb
Oscar Dulzaides <tuti@Oscars-MacBook-Pro.local>
2020-11-30 12:06:23 -0800

    Fixed: JS now works and is not duplicating rows.

--- main.js
+++ main.js
@@ -34,25 +34,36 @@ $(document).ready(function() { // ready
         viewData();
     }); // end save click event
 
-    // TODO - for loop to fill in table with row for each day. Limit to 2 weeks
+    // *** Append data to rows in table
+    // function appedData(date, weight, bmr, consumed, burned, hrv) {
+
+    // }
+
+    // TODO - Limit to 2 weeks
     function viewData() { // add data to table
         let dataTable = $('#data-table');
+        let rows = $('[data-type="data-row"]');
+        rows.remove();
+        let tableHeader = '<thead class="thead-dark"><tr><th>Date</th><th>Weight</th><th>BMR</th><th>Calories Consumed</th><th>Calories Burned</th><th>HRV Recovery</th><th>Calorie Balance</th></tr> </thead>';
+
+
+
         dataArray.forEach(function(i) {
             //  BMR and Calorie balance calculations for each day
             let bmrCalc = (88.3 + (13.4 * (i.weight / 2.2)) + (4.8 * (68 / 0.39)) - (5.7 * 60));
             let totalCalories = (bmrCalc + Math.floor(i.burned));
-            console.log(bmrCalc + i.burned, typeof(i.burned));
+            // console.log(bmrCalc + i.burned, typeof(i.burned));   
             let calorieBalance = i.consumed - totalCalories;
-            console.log("BMR, Total Calories, and Calorie Balance", typeof(bmrCalc), typeof(totalCalories), typeof(calorieBalance));
-            console.log("In viewData()", i.date, typeof(i.weight), typeof(i.consumed), i.burned, i.hrv);
 
-            // create new row.
-            let tr = '<tr><td>' + i.date + '</td> <td>' + Math.floor(i.weight) + '</td><td>' + bmrCalc +
-                '</td> <td>' + Math.floor(i.consumed) + '</td><td>' + Math.floor(i.burned) +
-                '</td><td>' + Math.floor(i.hrv) + '</td><td>' + calorieBalance + '</td></tr>';
+            // console.log("BMR, Total Calories, and Calorie Balance", typeof(bmrCalc), typeof(totalCalories), typeof(calorieBalance));
+            // console.log("In viewData()", i.date, typeof(i.weight), typeof(i.consumed), i.burned, i.hrv);
+
+            //    TODO - need to make this its own function and call it from this event
+            // TODO - add " rowspans these rows to make the size appropriate
+            let tr = '<tr data-type="data-row"><td >' + i.date + '</td> <td r>' + Math.floor(i.weight) + '</td><td >' + Math.round(bmrCalc) +
+                '</td> <td >' + Math.floor(i.consumed) + '</td><td>' + Math.floor(i.burned) +
+                '</td><td>' + Math.floor(i.hrv) + '</td><td>' + Math.round(calorieBalance) + '</td></tr>';
             dataTable.append(tr);
-            console.log(dataArray)
-
         });
     };
     // Add to local storage values to table /////
