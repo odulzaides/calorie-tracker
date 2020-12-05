2cce4eddbbc0b678d965c168e8884e8116a672dd
Oscar Dulzaides <tuti@Oscars-MacBook-Pro.local>
2020-12-04 19:12:08 -0800

    REFACTOR: Now using JSON-Server for datat. Can get data.

--- main.js
+++ main.js
@@ -2,14 +2,18 @@ $(document).ready(function() { // ready
 
     // global variables
     const save = $('#save');
-    let dataArray = localStorage.getItem('data') ? JSON.parse(localStorage.getItem('data')) : [];
-    localStorage.clear();
-    localStorage.setItem('data', JSON.stringify(dataArray));
-    const data = JSON.parse(localStorage.getItem("data"));
-    viewData();
+
+    //  ***** fetch data from server ****** ///////
+    // $.ajax('http://localhost:3000/DAYS', function(){
+    //     c
+    // })
+    let data = fetch('http://localhost:3000/DAYS')
+        .then(function(response) {
+            return response.json()
+        }).then(datar => viewData(datar))
+        // console.log("This  is outside of any function", data)
 
     // ******* events ********
-
     //  ///////  save button event ///////////
     $(save).click(function(e) { // save inputs to localStorage
         // e.preventDefault();
@@ -21,20 +25,20 @@ $(document).ready(function() { // ready
                 burned: $('#burned').val(),
                 hrv: $('#hrv').val()
             }
-            //add to dataArray and localStorage ...
-        dataArray.push(dayObject);
-        localStorage.setItem('data', JSON.stringify(dataArray));
-        // clear inputs
+            // // clear inputs
         $('#date').val('');
         $('#weight').val('');
         $('#consumed').val('');
         $('#burned').val('');
         $('#hrv').val('');
-        viewData();
+        // TODO - write a post function to save data
+        // viewData();
     }); // end save click event
+    //  xxxxxxx end events xxxxxxxxxxx
+
 
     // *** Append data to rows in table ********
-    function appedData(i) {
+    function appedData(i) { // add data to table
         let dataTable = $('#data-table');
 
         // TODO - add " rowspans these rows to make the size appropriate
@@ -46,16 +50,18 @@ $(document).ready(function() { // ready
 
 
     // TODO - !  Limit to 2 weeks and add an options 
-    function viewData(i) { // add data to table
+    function viewData(i) { // prepare data to add to table
         //clear table to add new data
         let rows = $('[data-type="data-row"]');
         rows.remove();
+        console.log("This is i", i);
 
-        dataArray.slice().reverse().forEach(function(i) {
-            //  BMR and Calorie balance calculations for each day
+        i.forEach(function(i) {
+            console.log("in loop", i)
+                //  BMR and Calorie balance calculations for each day
             let bmrCalc = (88.3 + (13.4 * (i.weight / 2.2)) + (4.8 * (68 / 0.39)) - (5.7 * 60));
             let totalCalories = (bmrCalc + Math.floor(i.burned));
-            // console.log(bmrCalc + i.burned, typeof(i.burned));   
+            // console.log(bmrCalc, i.burned, typeof(i.burned));
             let calorieBalance = i.consumed - totalCalories;
             let dayObj = {
                 date: i.date,
@@ -70,7 +76,24 @@ $(document).ready(function() { // ready
             appedData(dayObj);
         }); // ****** end viewData()
     };
-    //  end events ///////////
 
+
+    // console.log(response)
+
+    // fetch('http://localhost:3000/DAYS', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify({
+    //             date: "12/4/2020",
+    //             weight: 185,
+    //             calories: 1500,
+    //             caloriesBurned: 500,
+    //             hrv: 8
+    //         })
+    //     }).then(res => console.log(res))
+    //     .then(data => console.log(data))
+    //     .catch(error => console.log(error))
 
 }); // end ready...
