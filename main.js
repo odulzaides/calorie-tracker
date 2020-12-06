55ba263b281568f964fc989afd0f6ea36ba10a46
Oscar Dulzaides <tuti@Oscars-MacBook-Pro.local>
2020-12-06 14:18:44 -0800

    REFACTORED: Now saves data into json server file.

--- main.js
+++ main.js
@@ -3,18 +3,9 @@ $(document).ready(function() { // ready
     // global variables
     const save = $('#save');
 
-    //  ***** fetch data from server ****** ///////
-    // $.ajax('http://localhost:3000/DAYS', function(){
-    //     c
-    // })
-    let data = fetch('http://localhost:3000/DAYS')
-        .then(function(response) {
-            return response.json()
-        }).then(datar => viewData(datar))
-        // console.log("This  is outside of any function", data)
 
-    // ******* events ********
-    //  ///////  save button event ///////////
+    ///////// ////////// ////////     ----   E V E N T S  ----    //////// ////////////// /////////// //////
+    // save button event  
     $(save).click(function(e) { // save inputs to localStorage
         // e.preventDefault();
         // data object of inputs
@@ -31,13 +22,46 @@ $(document).ready(function() { // ready
         $('#consumed').val('');
         $('#burned').val('');
         $('#hrv').val('');
-        // TODO - write a post function to save data
-        // viewData();
+        saveInput(dayObject);
     }); // end save click event
-    //  xxxxxxx end events xxxxxxxxxxx
+    //xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxx end   E V E N T S  end  xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxx
+    ////////// ///////// //////// /////////         ---- C O N T R O L L E R ----        //////// /////////// /////////// /////////
+    //
+    // get data from server for diaplay
+    let data = fetch('http://localhost:3000/DAYS')
+        .then(function(response) {
+            return response.json()
+        }).then(datar => viewData(datar))
+
+    // Save day fitness input data *****  /////
+    let saveInput = function(i) {
+        console.log("2 saveInput", typeof(i), i);
+        console.log("saveInput POST", i)
+        fetch('http://localhost:3000/DAYS', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    date: i.date,
+                    weight: i.weight,
+                    consumed: i.consumed,
+                    burned: i.burned,
+                    hrv: i.hrv
+                })
+            }).then(res => console.log(res))
+            .then(data => console.log(data))
+            .catch(error => console.log(error))
+
+    }
+
+    //xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxx  end    C O N T R O L L E R  ----     end   xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxx
 
 
-    // *** Append data to rows in table ********
+    //// /////////// /////////// ///////////////         ---- V I E W ----        ///////// ///////////// ///////////// ////
+
+
+    // Append data to rows in table
     function appedData(i) { // add data to table
         let dataTable = $('#data-table');
 
@@ -49,12 +73,13 @@ $(document).ready(function() { // ready
     } // ***** end appedData()
 
 
-    // TODO - !  Limit to 2 weeks and add an options 
+
+    // Prepare data object to be displayed for each day
+    // TODO - !  Limit to 2 weeks and add an options
     function viewData(i) { // prepare data to add to table
         //clear table to add new data
         let rows = $('[data-type="data-row"]');
         rows.remove();
-        console.log("This is i", i);
 
         i.forEach(function(i) {
             console.log("in loop", i)
@@ -72,28 +97,9 @@ $(document).ready(function() { // ready
                 hrv: Math.floor(i.hrv),
                 calorieBalance: Math.round(calorieBalance)
             }
-            console.log(dayObj.calorieBalance);
             appedData(dayObj);
         }); // ****** end viewData()
     };
 
-
-    // console.log(response)
-
-    // fetch('http://localhost:3000/DAYS', {
-    //         method: 'POST',
-    //         headers: {
-    //             'Content-Type': 'application/json'
-    //         },
-    //         body: JSON.stringify({
-    //             date: "12/4/2020",
-    //             weight: 185,
-    //             calories: 1500,
-    //             caloriesBurned: 500,
-    //             hrv: 8
-    //         })
-    //     }).then(res => console.log(res))
-    //     .then(data => console.log(data))
-    //     .catch(error => console.log(error))
 
 }); // end ready...
