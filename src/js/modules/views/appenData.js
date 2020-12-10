0b72270bc8cd0bdeaf12e97a3ebfe32d010a1749
Oscar Dulzaides <tuti@Oscars-MacBook-Pro.local>
2020-12-10 13:05:02 -0800

    CSS: styled table and margins.

--- src/js/modules/views/appenData.js
+++ src/js/modules/views/appenData.js
@@ -1,8 +1,8 @@
 export function appendData(i) { // add data to table
     let dataTable = $('#data-table');
     // TODO - add " rowspans these rows to make the size appropriate
-    let tr = '<tr data-type="data-row"><td >' + i.date + '</td> <td r>' + Math.floor(i.weight) + '</td><td >' + Math.round(i.bmrCalc) +
-        '</td> <td >' + Math.floor(i.consumed) + '</td><td>' + Math.floor(i.burned) +
-        '</td><td>' + Math.floor(i.hrv) + '</td><td>' + Math.round(i.calorieBalance) + '</td><td><a href"#">edit</a></td></tr>';
+    let tr = '<tr data-type="data-row"><td class="m-0 p-0 pt-1 pb-1">' + i.date + '</td> <td class="m-0 p-0 pt-1 pb-1">' + Math.floor(i.weight) + '</td><td class="m-0 p-0 pt-1 pb-1">' + Math.round(i.bmrCalc) +
+        '</td> <td class="m-0 p-0 pt-1 pb-1">' + Math.floor(i.consumed) + '</td><td class="m-0 p-0 pt-1 pb-1">' + Math.floor(i.burned) +
+        '</td><td class="m-0 p-0 pt-1 pb-1">' + Math.floor(i.hrv) + '</td><td class="m-0 p-0 pt-1 pb-1">' + Math.round(i.calorieBalance) + '</td></tr>';
     dataTable.append(tr);
 }
